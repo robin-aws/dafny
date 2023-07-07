1a17263ad07fbb417de8b65531492f0d983eb885
Robin Salkeld <salkeldr@amazon.com>
2023-07-06 17:02:07 -0700

    Just starting

---
+++ Source/DafnyCore/Rewriters/AddCompileSuffixRewriter.cs
@@ -0,0 +1,11 @@
+namespace Microsoft.Dafny; 
+
+public class AddCompileSuffixRewriter : IRewriter {
+  internal AddCompileSuffixRewriter(ErrorReporter reporter) : base(reporter)
+  {
+  }
+  
+  internal virtual void PreResolve(ModuleDefinition moduleDefinition) {
+    if (moduleDefinition.)
+  }
+}
